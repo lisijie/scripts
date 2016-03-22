cfa59ec63a40a450dbe5f3fc0a1d8db7251c850b
sijie.li <lsj86@qq.com>
2016-03-22 10:36:40 +0800

    登录脚本

---
+++ go.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#----------------------
+# 登录到远程服务器
+# 用于使用SSH KEY登录
+# ---------------------
+
+ip=$1
+user=$2
+port=$3
+
+defUser="www"
+defPort="2222"
+
+while [[ "$ip" == "" ]]; do
+	read -p "请输入登录服务器(IP [user] [port])：" ip user port
+done
+
+if [ -z "$user" ]; then
+	user=$defUser
+fi
+
+if [ -z "$port" ]; then
+	port=$defPort
+fi
+
+# echo "ssh -p ${port} ${user}@${ip}"
+
+ssh -p ${port} ${user}@${ip}
